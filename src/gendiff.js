779bdffd0512094c26f775d8b322b2c670dfcc99
UruYoba <sotnikov97@mail.ru>
2021-02-01 08:25:46 +0300

    2nd step is done

--- src/gendiff.js
+++ src/gendiff.js
@@ -5,4 +5,6 @@ const program = new commander.Command();
 program
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1', '-v, --version', 'output the current output the version number')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format [type]', 'output format')
   .parse();
